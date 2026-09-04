f89d9cc11d987fa24b4cfc079836e07bc4ded246
Hyperhunk90 <87340064+Hyperhunk90@users.noreply.github.com>
2026-09-04 05:00:41 -0700

    feat(copy): slim homepage shell + HomePageBody composer

---
+++ src/components/HomePageBody.tsx
@@ -0,0 +1,13 @@
+import HomeHero from '@/components/HomeHero';
+import HomeMid from '@/components/HomeMid';
+import HomeTail from '@/components/HomeTail';
+
+export default function HomePageBody() {
+  return (
+    <>
+      <HomeHero />
+      <HomeMid />
+      <HomeTail />
+    </>
+  );
+}
